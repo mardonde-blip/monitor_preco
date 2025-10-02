3aa70033ed1c422511cfe8bd8b5983b2a8b774f6
Marcus Donde <mardonde@gmail.com>
2025-10-02 12:22:02 -0300

    next.config.js: adicionar alias '@' -> 'src' e ignorar ESLint no build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,26 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
   },
-  serverExternalPackages: ['pg']
-}
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  serverExternalPackages: ['pg'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push('pg-native');
+    }
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      ['@']: path.resolve(__dirname, 'src'),
+    };
+    return config;
+  }
+};
 
 module.exports = nextConfig
